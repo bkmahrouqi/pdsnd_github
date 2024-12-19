18f6390b442c3525142bcd2e51c4188a3d5852cd
Badar Almahrouqi <badar.almahrouqi@gmail.com>
2024-12-19 14:44:38 +0400

    Added a comment to bikeshare.py file

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,8 @@ import time
 import pandas as pd
 import numpy as np
 
+# This comment is added by Badar AL-Mahrouqi
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
